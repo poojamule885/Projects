0c8e7a855b3d76e4be8939d1c090061c2d547b24
poojamule885 <70598207+poojamule885@users.noreply.github.com>
2022-12-27 22:27:13 +0530

    Website - 1

---
+++ script.js
@@ -0,0 +1,11 @@
+let navbar=document.querySelector('.navbar');
+
+    function openWindow(){
+        window.open('https://en.wikipedia.org/wiki/Coffee');
+    }
+
+
+window .onscroll=()=>{
+    navbar.classList.remove('active');
+    
+}
