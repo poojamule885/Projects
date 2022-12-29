d2df7eef46064bd50b51c8a5543fcf07c5a38860
poojamule885 <70598207+poojamule885@users.noreply.github.com>
2022-12-29 21:21:03 +0530

    Website-1
    Used Html, Css, Javascript - Coffee website

---
+++ script.js
@@ -0,0 +1,16 @@
+let navbar=document.querySelector('.navbar');
+
+    function openWindow(){
+        window.open('https://en.wikipedia.org/wiki/Coffee');
+    }
+    function Openwindow(){
+        window.open('#products');
+    }
+    function OpenWindow(){
+        window.open('cu.jpg');
+    }
+
+window .onscroll=()=>{
+    navbar.classList.remove('active');
+    
+}
